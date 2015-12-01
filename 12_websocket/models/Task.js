b3d1c0e987a28e33b85500d92b1358aad1b37b57
dongseop <dongseop@gmail.com>
2015-12-01 11:21:07 +0900

    fix a bug

--- 12_websocket/models/Task.js
+++ 12_websocket/models/Task.js
@@ -4,8 +4,8 @@ var mongoose = require('mongoose'),
 
 var schema = new Schema({
   content: {type: String, required: true, trim: true},
-  category: {type: Number, trim: true},
-  priority: {type: String, trim: true},
+  category: {type: String, trim: true},
+  priority: {type: Number, trim: true},
   deadline: Date,
   done: {type: Boolean, default: false},
   user: {type: Schema.Types.ObjectId, index: true, required: true},
