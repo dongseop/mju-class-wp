3d9624e71b9f53ff51efdf265733084230826318
noname <noname@test.com>
2016-11-24 11:51:12 +0900

    change

--- 13_chat/public/javascripts/chat.js
+++ 13_chat/public/javascripts/chat.js
@@ -1,27 +1,34 @@
 $(function() {
   var template = _.template($('#template').html());
   var name = prompt('닉네임을 입력해주세요. 영어/숫자로만');
-  var socket = io('http://localhost:3000');
+  var socket = io({
+    upgrade: false,
+    transports: ['websocket']
+  });
   $('#name').val(name);
   socket.emit('join', name);
   socket.on('chat', function(data) {
     data.type = (data.from === name)? 'mine' : 'others';
     $('#messages').append(template(data));
+    $("html, body").animate({ scrollTop: $(document).height() }, "slow");
   });
 
   socket.on('dm', function(data) {
     data.type = 'direct';
     $('#messages').append(template(data));
+    $("html, body").animate({ scrollTop: $(document).height() }, "slow");
   })
   socket.on('join', function(name) {
     if (name) {
       $('#messages').append("<li><span class='text'>" + name + "님이 접속하셨습니다.</span></li>");
+      $("html, body").animate({ scrollTop: $(document).height() }, "slow");
     }
   });
 
   socket.on('left', function(name) {
     if (name) {
       $('#messages').append("<li><span class='text'>" + name + "님이 나갔습니다.</span></li>");
+      $("html, body").animate({ scrollTop: $(document).height() }, "slow");
     }
   });
 
